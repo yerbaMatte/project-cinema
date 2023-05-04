0246d22aa13a7cc297f1489419e8f60a694135bf
Milosz Lewandowski <milosz.lewandowski@icloud.com>
2023-05-04 23:05:48 +0200

    code

--- src/Hooks/hooks.ts
+++ src/Hooks/hooks.ts
@@ -4,5 +4,6 @@ import type { RootState, AppDispatch } from '../Store/store';
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 //
+//
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
